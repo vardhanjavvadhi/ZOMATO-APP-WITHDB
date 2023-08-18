a93bb4458f253a2f7a242475a3b5f7edbd1c6383
vardhanjavvadhi <134599754+vardhanjavvadhi@users.noreply.github.com>
2023-08-18 16:51:59 +0530

    first commit

---
+++ controller/RestaurantController.js
@@ -0,0 +1,50 @@
+const { request, response } = require("express");
+const RestaurantModel = require("../model/RestaurantModel");
+const MenuItemModel = require("../model/Menuitem");
+
+const RestaurantController = {
+  getRestaurantListByLocation: async (request, response) => {
+    let { loc_id } = request.params;
+    let result = await RestaurantModel.find(
+      { location_id: loc_id },
+      {
+        name: 1,
+        locality: 1,
+        image: 1,
+        city: 1,
+      }
+    );
+    response.send({
+      call: true,
+      result,
+    });
+  },
+  getSingleRestaurantDetails: async (request, response) => {
+    let { rest_id } = request.params;
+    let result = await RestaurantModel.findOne({ _id: rest_id });
+    response.send({
+      call: true,
+      result,
+    });
+  },
+
+  
+ getMenuItems : async(request , response)=>{
+   let {r_id} = request.params;
+   let result = await MenuItemModel.find({ restaurantId: r_id });
+   response.send({
+     call: true,
+     result,
+   });
+ },
+  
+  filter: async (request, response) => {
+    let result = await RestaurantModel.find();
+    response.send({
+      call: true,
+      result,
+    });
+  },
+};
+
+module.exports = RestaurantController;
